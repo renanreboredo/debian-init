76a3ec69636be3725210b49263d05a8dbd3a768d
Renan Reboredo <renanrreboredo@gmail.com>
2020-09-12 18:26:12 -0300

    Add auto install to i3 scripts

--- x.sh
+++ x.sh
@@ -1,13 +1,13 @@
 printf '\n\nInstall i3\n\n'
 sleep 2
-sudo apt install i3 xorg tint2 rofi
+sudo apt install i3 xorg tint2 rofi -y
 
 printf '\n\nInstall i3-gaps\n\n'
 sleep 2
 
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev \
 libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev  \
-libxkbcommon-dev libxkbcommon-x11-dev libxcb-xrm-dev autoconf xutils-dev libtool automake
+libxkbcommon-dev libxkbcommon-x11-dev libxcb-xrm-dev autoconf xutils-dev libtool automake -y
 
 cd ~/tmp
 git clone https://www.github.com/Airblader/i3 i3-gaps
