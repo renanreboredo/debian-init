6e2192a600f17810f3c4074d82f122df9088939d
Renan Reboredo <renanrreboredo@gmail.com>
2020-09-12 18:21:52 -0300

    Fix x script with rofi

--- x.sh
+++ x.sh
@@ -1,12 +1,12 @@
 printf '\n\nInstall i3\n\n'
 sleep 2
-sudo apt install i3 xorg tint2
+sudo apt install i3 xorg tint2 rofi
 
 printf '\n\nInstall i3-gaps\n\n'
 sleep 2
 
-sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev /
-libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev  / 
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev \
+libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev  \
 libxkbcommon-dev libxkbcommon-x11-dev libxcb-xrm-dev autoconf xutils-dev libtool automake
 
 cd ~/tmp
@@ -26,7 +26,9 @@ sleep 2
 cd ~/tmp 
 git clone https://github.com/renanreboredo/dotfiles.git
 cd dotfiles
+rm ~/.config/i3/config ~/.config/rofi/config 
 cp i3/config ~/.config/i3/config
+cp rofi/config ~/.config/rofi/config
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 echo 'exec i3' >> ~/.xinitrc
 mkdir ~/Pictures
